219cc8b92f2b44e1880e822e7a7063c220f37d9d
Akshay Kumar Sahu <166474581+akshay040503@users.noreply.github.com>
2026-02-14 12:20:19 +0530

    Add files via upload

---
+++ 10_primenumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int num = 4;
+
+    if ( num / num && num / 1){
+        cout << "This is a prime number ";
+    
+    }else {
+        cout << "This is not a prime number";
+    }
+
+
+    return 0;
+}
