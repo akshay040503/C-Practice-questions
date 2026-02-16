fbfc6127bbc3a580a426fec38e2c3d36baaccafd
Akshay Kumar Sahu <166474581+akshay040503@users.noreply.github.com>
2026-02-16 14:24:47 +0530

    Add files via upload

---
+++ 13_reversestring.cpp
@@ -0,0 +1,23 @@
+// C++ program to reverse a string using recursion 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void reverse(string str) 
+{ 
+    if(str.size() == 0)
+    {
+        return;
+    }
+    reverse(str.substr(1));
+    cout << str[0];
+} 
+
+
+int main() 
+{ 
+    string a = "Geeks for Geeks"; 
+    reverse(a); 
+    return 0; 
+} 
+
