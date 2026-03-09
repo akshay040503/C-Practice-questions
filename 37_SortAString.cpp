41985c2ec7a22493ab166442c9de25325fb4fade
Akshay Kumar Sahu <166474581+akshay040503@users.noreply.github.com>
2026-03-09 22:07:16 +0530

    Add files via upload

---
+++ 37_SortAString.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+
+    // C++ Style string
+    string str1 = "akshaykumar";
+  
+  	// C++ Style string
+  	char str2[] = "sahuakshay";
+  	int len2 = strlen(str2);
+
+    // Sort the strings using std::sort()
+    sort(str1.begin(), str1.end());
+  	sort(str2, str2 + len2);
+
+    cout << str1 << endl;
+  	cout << str2;
+
+
+
+    return 0;
+}
