39374f235a3bc19470700b6ec390c9f3dc14635d
Akshay Kumar Sahu <166474581+akshay040503@users.noreply.github.com>
2026-04-06 23:05:33 +0530

    Add files via upload

---
+++ 50_NumberMirror.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n) {
+    int rev = 0;
+
+    while(n > 0) {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+
+    return rev;
+}
+
+int main() {
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int reversed = reverseNumber(n);
+
+    int diff = abs(n - reversed);
+
+    cout << "Reversed Number: " << reversed << endl;
+    cout << "Difference: " << diff << endl;
+
+    return 0;
+}
